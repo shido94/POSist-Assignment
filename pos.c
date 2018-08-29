1a534abe04bd1531b1a84dafc8393ebf7e68b7e8
Rupesh yadav <rupeshyadav94.ry@gmail.com>
2018-08-29 16:06:24 +0530

    Add files via upload

--- pos.c
+++ pos.c
@@ -2,5 +2,28 @@
 
 
 struct node {
-	int 
+	char data;
+	int nodeNumber;
+	char nodeId;
+	char referenceNodeId;
+   char childReferenceNodeId;
+	char genesisReferenceNodeId;
+	char HashValue;
+	struct node *left;
+	struct node * right; 
 };
+
+int main(){
+	struct node* data = null;
+	struct node* nodeNumber = 1;
+}
+
+
+
+
+// create genesis node. genesis node has parent node as null
+// create set of child nodes of particular node
+// create a child node that originate from a particular node.
+// encryt and decrypt the data inside the node
+// verify the owner of node with the encryption key. symmetric encryption shows the data. the integrity of the data is checked by computing the hash value of the data and checking with the already 
+
